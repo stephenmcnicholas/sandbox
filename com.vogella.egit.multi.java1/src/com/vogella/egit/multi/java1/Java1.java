4f698702fceaf744e2cc619f2a379911669fc493
Stephen McNicholas <stephenmcnicholas@hotmail.com>
2018-10-01 14:58:15 +0100

    Everything in Java1 and Java2 projects

---
+++ com.vogella.egit.multi.java1/src/com/vogella/egit/multi/java1/Java1.java
@@ -0,0 +1,10 @@
+package com.vogella.egit.multi.java1;
+
+public class Java1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Java 1");
+	}
+
+}
---
+++ com.vogella.egit.multi.java2/src/com/vogella/egit/multi/java2/Java2.java
@@ -0,0 +1,10 @@
+package com.vogella.egit.multi.java2;
+
+public class Java2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Java 2");
+	}
+
+}
