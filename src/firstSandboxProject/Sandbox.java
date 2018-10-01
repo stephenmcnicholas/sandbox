213cdb6f6d8028b6826b2ead16c49fc660bd22db
Stephen McNicholas <stephenmcnicholas@hotmail.com>
2018-10-01 12:38:02 +0100

    print hello sandbox

---
+++ src/firstSandboxProject/Sandbox.java
@@ -0,0 +1,10 @@
+package firstSandboxProject;
+
+public class Sandbox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello sandbox");
+	}
+
+}
