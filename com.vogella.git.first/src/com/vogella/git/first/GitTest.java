f49136da39b39afcba33e1ba436b8eda6048b4fc
Stephen McNicholas <stephenmcnicholas@hotmail.com>
2018-10-01 14:49:42 +0100

    updated print message from "Fun" to "Cool"

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git is Fun!");
+		System.out.println("Git is Cool!");
 	}
 }
